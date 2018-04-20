e140e2e3776ef3ff4f4a1180443589f632cd639e
Mark Schlax <mcschlax@mtu.edu>
2018-04-20 12:48:33 -0400

    Add files via upload

---
+++ helloWorld.py
@@ -0,0 +1,8 @@
+# Mark Schlax
+# September 15, 2010
+# helloWorld.py
+# This program will print "Hello, World!" then disappear after the user hits a key.
+
+print "Hello, World"
+
+raw_input ("Press <enter> to exit.")
